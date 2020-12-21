ddf7b7808772a8d59b6645e2c26a97c327620427
SedgeCrystal <61766231SedgeCrystal@users.noreply.github.com>
2020-12-21 14:29:50 +0900

    Customizing InputField.
    Adding CustomInputField class  and InputFieldController class.
    
    Default InputField cannot be prevented from selecting all text on end edit.
    This function is not suitable for typing game, because we have to click InputField to type next line after pressing enter.

---
+++ Assets/Scripts/CustomInputField.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomInputField : InputField
+{
+    public bool isAfterSent = false;
+    public int lastCaretPos = 0;
+    protected override void LateUpdate()
+    {
+       
+        base.LateUpdate();
+
+        if (isAfterSent)
+        {
+            Debug.Log(lastCaretPos);
+            MoveTextEnd(false);
+            this.selectionAnchorPosition = this.lastCaretPos;
+            this.selectionFocusPosition = this.lastCaretPos;
+            isAfterSent = false;
+        }
+    }
+}
---
+++ Assets/Scripts/InputFieldController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputFieldController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    CustomInputField customInputField;
+   
+    
+    
+    void Start()
+    {
+        this.customInputField = gameObject.GetComponent<CustomInputField>();
+        this.customInputField.ActivateInputField();
+
+    }
+
+    public void InputText()
+    {
+
+        
+        this.customInputField.ActivateInputField();
+
+        this.customInputField.lastCaretPos = this.customInputField.caretPosition;
+        this.customInputField.isAfterSent = true;
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+}
+
+
