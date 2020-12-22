8cc142c9c3a23811ae3598b2fa4ea87a61322732
SedgeCrystal <61766231SedgeCrystal@users.noreply.github.com>
2020-12-22 19:34:04 +0900

    Adding feature to change ExampleText.
    If isCorrect in GameDirecotr is ture, ExmapleText shows next text.

---
+++ Assets/Scripts/ExampleTextController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExampleTextController : MonoBehaviour
+{
+    const int MAX_ROW = 4;
+
+    Text exampleText;
+    
+
+    GameDirector gameDirector;
+    void Start()
+    {
+        this.exampleText = gameObject.GetComponent<Text>();
+        
+        GameObject gameDirectorObject = GameObject.FindGameObjectWithTag("GameDirector");
+        this.gameDirector = gameDirectorObject.GetComponent<GameDirector>();
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetText(List<string> list,int line)
+    {
+        this.exampleText.text = ArrangeText(list,line);
+        
+    }
+
+    string ArrangeText(List<string> list,int line)
+    {
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < MAX_ROW; i++)
+        {
+            if(line >= list.Count)
+            {
+                break;
+            }
+            sb.AppendLine(list[line]);
+            sb.AppendLine();
+            line++;
+        }
+        return sb.ToString();
+    }
+}
--- Assets/Scripts/GameDirector.cs
+++ Assets/Scripts/GameDirector.cs
@@ -8,16 +8,36 @@ public class GameDirector : MonoBehaviour
 
     public bool isCorrect = false;
     InputFieldController inputFieldController;
-    
+    ExampleTextController exampleTextController;
 
-    string s = "aaaa";
+    int line;
+    int MAX_LINE;
 
+    public List<string> exampleTextList;
+   
+
+   
+
+    private void Awake()
+    {
+        this.line = 0;
+
+        this.exampleTextList = new List<string>() { "aaa", "bbb","ccc","ddd","eee"};
+        MAX_LINE = exampleTextList.Count;
+
+
+    }
     void Start()
     {
         GameObject inputFieldObject = GameObject.FindGameObjectWithTag("InputField");
         this.inputFieldController = inputFieldObject.GetComponent<InputFieldController>();
-        this.inputFieldController.exmStr = s;
+        this.inputFieldController.exmStr = this.exampleTextList[line];
 
+
+
+        GameObject exampleTextObject = GameObject.FindGameObjectWithTag("ExampleText");
+        this.exampleTextController = exampleTextObject.GetComponent<ExampleTextController>();
+        this.exampleTextController.SetText(exampleTextList,line);
         
     }
 
@@ -29,7 +49,19 @@ public class GameDirector : MonoBehaviour
 
         if(this.isCorrect)
         {
-            Debug.Log("Correct!");
+            line++;
+            Debug.Log(line);
+            if (this.line >= MAX_LINE)
+            {
+                Debug.Log("END");
+
+            }
+            else
+            {
+                this.inputFieldController.exmStr = this.exampleTextList[line];
+                this.exampleTextController.SetText(exampleTextList, line);
+                
+            }
             this.isCorrect = false;
         }
 
