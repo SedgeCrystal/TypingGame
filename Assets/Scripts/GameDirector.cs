a6b356f863c8c766e9799929653a2564646057d0
SedgeCrystal <61766231SedgeCrystal@users.noreply.github.com>
2020-12-23 14:41:10 +0900

    Adding feature to set text.
    This feature can set TitleText, TimerText and LineText.
    
    This feature is in GameDirector not their Controller scripts because these texts only need simple features.

--- Assets/Scripts/GameDirector.cs
+++ Assets/Scripts/GameDirector.cs
@@ -9,12 +9,18 @@ public class GameDirector : MonoBehaviour
     public bool isCorrect = false;
     InputFieldController inputFieldController;
     ExampleTextController exampleTextController;
+    
+    Text timerText;
+    Text lineText;
+    Text titleText;
 
+    float time;
     int line;
+    
     int MAX_LINE;
 
     public List<string> exampleTextList;
-   
+    string title;
 
    
 
@@ -25,29 +31,49 @@ public class GameDirector : MonoBehaviour
         this.exampleTextList = new List<string>() { "aaa", "bbb","ccc","ddd","eee"};
         MAX_LINE = exampleTextList.Count;
 
-
+        this.time = -3;
+        this.title = "Sample";
     }
     void Start()
     {
         GameObject inputFieldObject = GameObject.FindGameObjectWithTag("InputField");
         this.inputFieldController = inputFieldObject.GetComponent<InputFieldController>();
         this.inputFieldController.exmStr = this.exampleTextList[line];
-
+        
 
 
         GameObject exampleTextObject = GameObject.FindGameObjectWithTag("ExampleText");
         this.exampleTextController = exampleTextObject.GetComponent<ExampleTextController>();
         this.exampleTextController.SetText(exampleTextList,line);
-        
+
+        GameObject timerTextObject = GameObject.FindGameObjectWithTag("TimerText");
+        this.timerText = timerTextObject.GetComponent<Text>();
+
+        GameObject lineTextObject = GameObject.FindGameObjectWithTag("LineText");
+        this.lineText = lineTextObject.GetComponent<Text>();
+
+        GameObject titleTextObject = GameObject.FindGameObjectWithTag("TitleText");
+        this.titleText = titleTextObject.GetComponent<Text>();
+        this.titleText.text = this.title;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        this.time += Time.deltaTime;
         
+        if(time < 0)
+        {
+
+            return; 
+        }
+        this.timerText.text = time.ToString("F1");
 
 
-        if(this.isCorrect)
+        this.lineText.text = string.Format("{0:00}/{1:00}", line, MAX_LINE);
+
+        if (this.isCorrect)
         {
             line++;
             Debug.Log(line);
