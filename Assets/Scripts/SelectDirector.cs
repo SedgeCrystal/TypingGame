0fe27e5c9442ba98287961590ea51d8d78833570
SedgeCrystal <61766231SedgeCrystal@users.noreply.github.com>
2020-12-25 17:49:51 +0900

    Adding feature to select path.
    Path can be selected by InputField and Browser Dialog.

---
+++ Assets/Scripts/SelectDirector.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Windows.Forms;
+
+public class SelectDirector : MonoBehaviour
+{
+
+    InputField inputField;
+    
+    // Start is called before the first frame update
+    string path;
+
+
+    OpenFileDialog openFileDialog;
+    void Start()
+    {
+        path = "C:\\";
+        GameObject inputFieldObject = GameObject.FindWithTag("InputField");
+        this.inputField = inputFieldObject.GetComponent<InputField>();
+        this.inputField.text = path;
+    
+        this.openFileDialog = new OpenFileDialog();       
+        this.openFileDialog.Filter = "txt files (*.txt)|*.txt";
+        this.openFileDialog.FileName = path;
+        
+        this.openFileDialog.CheckFileExists = true;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+       
+
+   
+    }
+
+    public void OnClickBrowseButton()
+    {
+        
+        if (this.openFileDialog.ShowDialog() == DialogResult.OK)
+        {
+            this.path = this.openFileDialog.FileName;
+        }
+        
+        this.inputField.text = path;
+
+        
+    } 
+
+    public void OnEndPathEdit()
+    {
+        this.path = inputField.text;
+        this.openFileDialog.FileName = this.path;
+    }
+
+   
+}
