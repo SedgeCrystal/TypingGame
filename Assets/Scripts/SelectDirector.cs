9d65d5f7837118e64d1673617921489f0d436e95
SedgeCrystal <61766231SedgeCrystal@users.noreply.github.com>
2020-12-26 17:45:36 +0900

    Adding feature to save last parh.
    Path is saved by PlayerPref.

--- Assets/Scripts/SelectDirector.cs
+++ Assets/Scripts/SelectDirector.cs
@@ -9,195 +9,235 @@ using UnityEngine.SceneManagement;
 
 public class SelectDirector : MonoBehaviour
 {
-
+    //input path
     InputField inputField;
-    
-    // Start is called before the first frame update
+
+    //path to type
     public string path;
 
     //the state of this Scene
-    //0:unable to start
-    //1:able to start
-    //2:error occur while read file
+    //State is defined at bottom of this file.
     State state;
+
     Text noticeText;
+
+    //error message
     string errMsg;
 
+    //max character in one line
     const int MAX_CHAR = 50;
 
     OpenFileDialog openFileDialog;
 
+    //List of example text to type.
     public List<string> exmList;
 
 
     void Start()
     {
-        path = "C:\\";
+        
+
         GameObject inputFieldObject = GameObject.FindWithTag("InputField");
         this.inputField = inputFieldObject.GetComponent<InputField>();
-        this.inputField.text = path;
+        
 
         GameObject noticeTextObject = GameObject.FindWithTag("NoticeText");
         this.noticeText = noticeTextObject.GetComponent<Text>();
 
-        this.openFileDialog = new OpenFileDialog();       
+        this.openFileDialog = new OpenFileDialog();
+        //only txt file can be chosen
         this.openFileDialog.Filter = "txt files (*.txt)|*.txt";
-        this.openFileDialog.FileName = path;
-        
+
+        this.LoadPathData();
+        this.ChangePath(this.path);
+
         this.openFileDialog.CheckFileExists = true;
+
 
         this.exmList = new List<string>();
 
-
+        //This object have to give information about selected file to GameDirector.
         DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        Debug.Log(this.state);
+
         CheckPath();
         UpdateNoticeText();
-       
+
     }
-
-    public void OnClickBrowseButton()
+    void LoadPathData()
     {
-        
-        if (this.openFileDialog.ShowDialog() == DialogResult.OK)
+        if (PlayerPrefs.HasKey("Path"))
         {
-            this.path = this.openFileDialog.FileName;
+            this.path = PlayerPrefs.GetString("Path");
         }
-        
-        this.inputField.text = path;
-
-        
-    } 
+        else
+        {
+            this.path = "C:\\";
+        }
+    }
 
     public void OnClickStartButton()
     {
         this.ReadText();
-        
-        
 
-        Debug.Log(this.exmList.Count);
-        
 
         this.CheckExmList();
 
-        if(this.state == State.OK)
+        if (this.state == State.OK)
         {
+            PlayerPrefs.SetString("Path", this.path);
+            PlayerPrefs.Save();
             SceneManager.LoadScene("GameScene");
         }
     }
 
     void ReadText()
     {
-        if (state == State.NotExist)
+        //if state is State.InvalidPath, do nothing.
+        if (state == State.InvalidPath)
         {
             return;
         }
         try
         {
-            
+
             using (StreamReader sr = new StreamReader(path))
             {
                 while (sr.Peek() != -1)
                 {
                     string line = sr.ReadLine();
                     line = line.Trim();
-                    if(line.Length == 0)
+                    if (line.Length == 0)
                     {
                         continue;
                     }
                     this.exmList.Add(line);
                 }
             }
-            
+
         }
         catch (Exception e)
         {
             state = State.Exception;
-            this.errMsg = $"{e.GetType()} has occurred.";
+            //get type of exception.
+            this.errMsg = $"{e.GetType()} has occurred!";
         }
 
     }
-    
+
     void CheckExmList()
     {
-        if (state == State.NotExist)
+        //if state is State.InvalidPath, do nothing.
+        if (state == State.InvalidPath)
         {
             return;
         }
 
+        //Check exmList is empty
         if (this.exmList.Count == 0)
         {
             this.state = State.Empty;
             return;
         }
 
-        foreach(string exm in exmList)
+        //Check elements of exmList contains many characters.
+        foreach (string exm in exmList)
         {
-            if(exm.Length > SelectDirector.MAX_CHAR)
+            if (exm.Length > SelectDirector.MAX_CHAR)
             {
                 this.state = State.OverMaxChar;
                 return;
             }
-            {
 
-            }
         }
     }
 
-    public void OnEndPathEdit()
-    {
-        this.path = inputField.text;
-        this.openFileDialog.FileName = this.path;
-    }
+
 
     void CheckPath()
     {
-        if(this.state != State.OK && this.state !=State.NotExist)
+        //if state isn't State.OK abd State.InvalidPath, do nothing.
+        if (this.state != State.OK && this.state != State.InvalidPath)
         {
             return;
         }
 
+
         if (!File.Exists(this.path))
         {
-            this.state = State.NotExist;
+            this.state = State.InvalidPath;
             return;
         }
 
         string ext = Path.GetExtension(this.path);
-        this.state = (ext == ".txt") ? State.OK : State.NotExist;
+        this.state = (ext == ".txt") ? State.OK : State.InvalidPath;
     }
 
     void UpdateNoticeText()
     {
 
         string message = "";
+
         switch (this.state)
         {
-            case State.NotExist: message = "THIS PATH IS NOT POITING TEXT FILE!";
+            case State.OK:
+                message = "";
                 break;
-            case State.Exception: message = this.errMsg;
+            case State.InvalidPath:
+                message = "This path is not pointing text file!";
                 break;
-            case State.Empty: message = "THIS FILE IS EMPTY";
+            case State.Exception:
+                message = this.errMsg;
+                break;
+            case State.Empty:
+                message = "This file is empty!";
                 break;
             case State.OverMaxChar:
-                message = "THIS FILE CONTAINS TOO MANY CHARACTERS";
+                message = "This file contains too many character in one line!";
                 break;
         }
-
+        
         this.noticeText.text = message;
     }
-   
+
+    public void OnClickBrowseButton()
+    {
+
+        if (this.openFileDialog.ShowDialog() == DialogResult.OK)
+        {
+            this.ChangePath(this.openFileDialog.FileName);
+        }
+    }
+    public void InputText()
+    {
+        this.ChangePath(this.inputField.text);
+
+    }
+
+    void ChangePath(string newPath)
+    {
+        this.path = newPath;
+
+        this.openFileDialog.FileName = this.path;
+        this.inputField.text = this.path;
+
+        //If path is changed, state isn't Exception, Empty or OverMaxChar;
+        this.state = State.InvalidPath;
+    }
+
+
+
+
 }
 
 enum State
 {
     OK,
-    NotExist,
+    InvalidPath,
     Exception,
     Empty,
     OverMaxChar
